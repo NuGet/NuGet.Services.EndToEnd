a1defe9846d4b904d1af85d2047640408ddcb2b5
Joel Verhagen <jver@microsoft.com>
2017-04-20 09:50:33 -0700

    Make generic function for getting the base URL from Azure

--- scripts/Configure-Environment.ps1
+++ scripts/Configure-Environment.ps1
@@ -2,44 +2,69 @@
 param (
     [string]$GalleryBaseUrl,
     [ValidateSet("Production", "Staging", "", IgnoreCase=$true)]
-    [string]$Slot,
-    [string]$CloudServiceName,
+    [string]$GallerySlot,
+    [string]$GalleryCloudServiceName,
     [string]$SubscriptionId,
     [string]$ApplicationId,
     [string]$TenantId,
     [string]$AzureCertificateThumbprint
 )
 
-if ($Slot -eq "Staging")
+Function Get-BaseUrl
 {
-    # Use Azure PowerShell cmdlets to find the URL of the staging slot.
-    Try
+    param (
+        [string]$ApplicationId,
+        [string]$AzureCertificateThumbprint,
+        [string]$SubscriptionId,
+        [string]$TenantId,
+        [string]$BaseUrl,
+        [ValidateSet("Production", "Staging", "", IgnoreCase=$true)]
+        [string]$Slot,
+        [string]$CloudServiceName
+    )
+
+    if ($Slot -eq "Staging")
     {
-        Write-Host "Logging into Azure as service principal."
+        # Use Azure PowerShell cmdlets to find the URL of the staging slot.
+        Try
+        {
+            Write-Host "Logging into Azure as service principal."
 
-        $login = Add-AzureRmAccount `
-            -ApplicationId $ApplicationId `
-            -CertificateThumbprint $AzureCertificateThumbprint `
-            -ServicePrincipal `
-            -SubscriptionId $SubscriptionId `
-            -TenantId $TenantId
+            $login = Add-AzureRmAccount `
+                -ApplicationId $ApplicationId `
+                -CertificateThumbprint $AzureCertificateThumbprint `
+                -ServicePrincipal `
+                -SubscriptionId $SubscriptionId `
+                -TenantId $TenantId
 
-        Write-Host "Finding cloud service resource '$CloudServiceName'."
-        $resourceGroupName = (Find-AzureRmResource -ResourceNameEquals $CloudServiceName).ResourceGroupName
+            Write-Host "Finding cloud service resource '$CloudServiceName'."
+            $resourceGroupName = (Find-AzureRmResource -ResourceNameEquals $CloudServiceName).ResourceGroupName
 
-        Write-Host "Get cloud service resource details for slot '$Slot'."
-        $slotResource = Get-AzureRmResource `
-            -Id "/subscriptions/$SubscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.ClassicCompute/domainNames/$CloudServiceName/slots/$Slot"
-        
-        $GalleryBaseUrl = ($slotResource.Properties.uri).Replace("http", "https")
+            Write-Host "Get cloud service resource details for slot '$Slot'."
+            $slotResource = Get-AzureRmResource `
+                -Id "/subscriptions/$SubscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.ClassicCompute/domainNames/$CloudServiceName/slots/$Slot"
+            
+            $BaseUrl = ($slotResource.Properties.uri).Replace("http", "https")
+        }
+        Catch [System.Exception]
+        {
+            Write-Host "Failed to retrieve URL for testing!"
+            Write-Host $_.Exception.Message
+            Exit 1
+        }
     }
-    Catch [System.Exception]
-    {
-        Write-Host "Failed to retrieve URL for testing!"
-        Write-Host $_.Exception.Message
-        Exit 1
-    }
+
+    return $BaseUrl
 }
+
+$GalleryBaseUrl = Get-BaseUrl `
+    -ApplicationId $ApplicationId `
+    -AzureCertificateThumbprint $AzureCertificateThumbprint `
+    -SubscriptionId $SubscriptionId `
+    -TenantId $TenantId `
+    -BaseUrl $GalleryBaseUrl `
+    -Slot $GallerySlot `
+    -CloudServiceName $GalleryCloudServiceName
 
 Write-Host "Using the following GalleryBaseUrl: $GalleryBaseUrl" 
 $env:GalleryBaseUrl = $GalleryBaseUrl
