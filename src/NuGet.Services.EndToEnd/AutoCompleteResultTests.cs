fd9d68a03b76507bc42ce4827d0911ad638678f7
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2017-06-16 10:38:04 +0200

    Merging E2E tests for autocomplete endpoints into master (#14)

---
+++ src/NuGet.Services.EndToEnd/AutoCompleteResultTests.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using NuGet.Services.EndToEnd.Support;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NuGet.Services.EndToEnd
+{
+    [Collection(nameof(PushedPackagesCollection))]
+    public class AutocompleteResultTests
+        : IClassFixture<TrustedHttpsCertificatesFixture>
+    {
+        private readonly PushedPackagesFixture _pushedPackages;
+        private readonly TestSettings _testSettings;
+        private readonly ITestOutputHelper _logger;
+        private readonly Clients _clients;
+
+        public AutocompleteResultTests(PushedPackagesFixture pushedPackages, ITestOutputHelper logger)
+        {
+            _pushedPackages = pushedPackages;
+            _clients = Clients.Initialize();
+            _testSettings = TestSettings.Create();
+            _logger = logger;
+        }
+
+        [Theory]
+        [InlineData(PackageType.SemVer1Stable, true, null)]
+        [InlineData(PackageType.SemVer1Stable, false, null)]
+        [SemVer2InlineData(PackageType.SemVer2Prerel, true, "2.0.0")]
+        [SemVer2InlineData(PackageType.SemVer2StableMetadata, false, "2.0.0")]
+        public async Task V2PackageVersionsAutocompleteResultsMatchV3Results(
+            PackageType packageType,
+            bool includePrerelease,
+            string semVerLevel)
+        {
+            // Arrange
+            var package = await _pushedPackages.PrepareAsync(packageType, _logger);
+
+            // Act
+            var v2Response = await _clients.Gallery.AutocompletePackageVersionsAsync(
+                package.Id, 
+                includePrerelease, 
+                semVerLevel, 
+                _logger);
+
+            foreach (var v3Endpoint in await _clients.V2V3Search.GetSearchBaseUrlsAsync())
+            {
+                var v3Response = await _clients.V2V3Search.AutocompletePackageVersionsAsync(
+                    v3Endpoint,
+                    package.Id,
+                    includePrerelease,
+                    semVerLevel,
+                    _logger);
+                
+                // Assert
+                Assert.Equal(v2Response.Count, v3Response.Data.Count);
+                Assert.Equal(v2Response, v3Response.Data);
+            }
+        }
+
+        [Theory]
+        [InlineData(PackageType.SemVer1Stable, true, null)]
+        [InlineData(PackageType.SemVer1Stable, false, null)]
+        [SemVer2InlineData(PackageType.SemVer2Prerel, true, "2.0.0")]
+        [SemVer2InlineData(PackageType.SemVer2StableMetadata, false, "2.0.0")]
+        public async Task V2PackageIdsAutocompleteResultsMatchV3Results(
+            PackageType packageType,
+            bool includePrerelease,
+            string semVerLevel)
+        {
+            // Arrange
+            var package = await _pushedPackages.PrepareAsync(packageType, _logger);
+
+            // Act
+            var v2Response = await _clients.Gallery.AutocompletePackageIdsAsync(
+                package.Id,
+                includePrerelease,
+                semVerLevel,
+                _logger);
+
+            foreach (var v3Endpoint in await _clients.V2V3Search.GetSearchBaseUrlsAsync())
+            {
+                var v3Response = await _clients.V2V3Search.AutocompletePackageIdsAsync(
+                    v3Endpoint,
+                    package.Id,
+                    includePrerelease,
+                    semVerLevel,
+                    _logger);
+
+                // Assert
+                Assert.Equal(v2Response.Count, v3Response.Data.Count);
+                Assert.Equal(v2Response, v3Response.Data);
+            }
+        }
+    }
+}
--- src/NuGet.Services.EndToEnd/Support/Clients/IGalleryClient.cs
+++ src/NuGet.Services.EndToEnd/Support/Clients/IGalleryClient.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
 
 namespace NuGet.Services.EndToEnd.Support
 {
@@ -11,5 +13,7 @@ namespace NuGet.Services.EndToEnd.Support
         Task PushAsync(Stream nupkgStream);
         Task UnlistAsync(string id, string version);
         Task RelistAsync(string id, string version);
+        Task<IList<string>> AutocompletePackageIdsAsync(string id, bool includePrerelease, string semVerLevel, ITestOutputHelper logger);
+        Task<IList<string>> AutocompletePackageVersionsAsync(string id, bool includePrerelease, string semVerLevel, ITestOutputHelper logger);
     }
 }
