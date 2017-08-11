6b3b03b98ec9a17eebbfee7d40716c33907502ed
Svetlana Kofman <skofman@microsoft.com>
2017-08-11 16:25:27 -0700

    Add import config script

---
+++ importconfig.ps1
@@ -0,0 +1,17 @@
+[CmdletBinding()]
+param (
+    [string]$Instance,
+    [string]$Project,
+    [string]$PersonalAccessToken,
+    [string]$Repository,
+	[string]$FileName,
+    [string]$OutputDirectory
+)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$basicAuth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f 'PAT', $PersonalAccessToken)))
+$headers = @{ Authorization = ("Basic {0}" -f $basicAuth) }
+
+$requestUri = "https://$Instance.visualstudio.com/DefaultCollection/$Project/_apis/git/repositories/$Repository/items?api-version=1.0&scopePath={$FileName}"
+Invoke-WebRequest -UseBasicParsing -Uri $requestUri -OutFile "$OutputDirectory/$FileName" 
