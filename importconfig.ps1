5423fea4fb3c1321ca71af867671ecf44acdb473
Svetlana Kofman <skofman@microsoft.com>
2017-08-14 14:21:09 -0700

    Fix

--- importconfig.ps1
+++ importconfig.ps1
@@ -5,7 +5,8 @@ param (
     [string]$PersonalAccessToken,
     [string]$Repository,
 	[string]$FileName,
-    [string]$OutputPath
+    [string]$OutputDirectory,
+	[string]$OutputFile
 )
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -16,4 +17,4 @@ $basicAuth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}
 $headers = @{ Authorization = ("Basic {0}" -f $basicAuth) }
 
 $requestUri = "https://$Instance.visualstudio.com/DefaultCollection/$Project/_apis/git/repositories/$Repository/items?api-version=1.0&scopePath={$FileName}"
-Invoke-WebRequest -UseBasicParsing -Uri $requestUri -OutFile "$OutputPath" 
+Invoke-WebRequest -UseBasicParsing -Uri $requestUri -OutFile "$OutputDirectory\$OutputFile" 
