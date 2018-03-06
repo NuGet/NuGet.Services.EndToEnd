760bcffce574bfe9796ef945734a3a27aec2ed65
Svetlana Kofman <skofman@microsoft.com>
2018-03-05 18:37:19 -0800

    fix typo

--- src/NuGet.Services.EndToEnd/AutoCompleteResultTests.cs
+++ src/NuGet.Services.EndToEnd/AutoCompleteResultTests.cs
@@ -23,7 +23,7 @@ namespace NuGet.Services.EndToEnd
             _logger = logger;
         }
 
-        //[GalleryTestTheory] Uncomment this line, and remove the Theory bellow once the test is fixed
+        //[GalleryTestTheory] Uncomment this line, and remove the Theory below once the test is fixed
         [Theory(Skip = "Flaky, tracked https://github.com/NuGet/NuGetGallery/issues/5407")]
         [InlineData(PackageType.SemVer1Stable, true, null)]
         [InlineData(PackageType.SemVer1Stable, false, null)]
@@ -65,7 +65,7 @@ namespace NuGet.Services.EndToEnd
             }
         }
 
-        //[GalleryTestTheory] Uncomment this line, and remove the Theory bellow once the test is fixed
+        //[GalleryTestTheory] Uncomment this line, and remove the Theory below once the test is fixed
         [Theory(Skip = "Flaky, tracked https://github.com/NuGet/NuGetGallery/issues/5407")]
         [InlineData(PackageType.SemVer1Stable, true, null)]
         [InlineData(PackageType.SemVer1Stable, false, null)]
