2185d951736cb61a5bfb3e883b956802fcb94444
Peter Yu <ryuyu@microsoft.com>
2018-03-07 12:09:00 -0800

    Re-enable autocomplete tests.

--- src/NuGet.Services.EndToEnd/AutoCompleteResultTests.cs
+++ src/NuGet.Services.EndToEnd/AutoCompleteResultTests.cs
@@ -23,8 +23,7 @@ namespace NuGet.Services.EndToEnd
             _logger = logger;
         }
 
-        //[GalleryTestTheory] Uncomment this line, and remove the Theory below once the test is fixed
-        [Theory(Skip = "Flaky, tracked https://github.com/NuGet/NuGetGallery/issues/5407")]
+        [GalleryTestTheory]
         [InlineData(PackageType.SemVer1Stable, true, null)]
         [InlineData(PackageType.SemVer1Stable, false, null)]
         [InlineData(PackageType.SemVer2Prerel, true, "2.0.0")]
@@ -65,8 +64,7 @@ namespace NuGet.Services.EndToEnd
             }
         }
 
-        //[GalleryTestTheory] Uncomment this line, and remove the Theory below once the test is fixed
-        [Theory(Skip = "Flaky, tracked https://github.com/NuGet/NuGetGallery/issues/5407")]
+        [GalleryTestTheory]
         [InlineData(PackageType.SemVer1Stable, true, null)]
         [InlineData(PackageType.SemVer1Stable, false, null)]
         [InlineData(PackageType.SemVer2Prerel, true, "2.0.0")]
