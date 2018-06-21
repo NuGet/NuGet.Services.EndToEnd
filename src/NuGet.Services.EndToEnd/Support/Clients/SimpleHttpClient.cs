5d8ed6a7bdca840387f58e687743e192887b2ebe
Damon Tivel <dtivel@microsoft.com>
2018-06-21 12:09:11 -0700

    Apply feedback.

--- src/NuGet.Services.EndToEnd/Support/Clients/SimpleHttpClient.cs
+++ src/NuGet.Services.EndToEnd/Support/Clients/SimpleHttpClient.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NuGet.Protocol;
 using Xunit.Abstractions;
 
 namespace NuGet.Services.EndToEnd.Support
