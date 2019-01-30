9093e3204774ca0fa8efd5486cf3f78d2fd735a4
zhhyu <Zhaohua.Yu@microsoft.com>
2019-01-29 18:13:28 -0800

    Add search test for licenses

--- src/NuGet.Services.EndToEnd/LicenseTests.cs
+++ src/NuGet.Services.EndToEnd/LicenseTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using NuGet.Services.EndToEnd.Support;
 using Xunit;
 using Xunit.Abstractions;
@@ -39,6 +40,7 @@ namespace NuGet.Services.EndToEnd
             Assert.All(packageRegistrationList, x => Assert.Equal(package.Properties.LicenseMetadata.License, x.CatalogEntry.LicenseExpression));
             Assert.All(packageRegistrationList, x => Assert.Equal(expectedPath, new Uri(x.CatalogEntry.LicenseUrl).AbsolutePath));
             await _clients.FlatContainer.WaitForPackageAsync(package.Id, package.NormalizedVersion, _logger);
+            VerifyLicenseUrlForV2V3Search(package, expectedPath);
         }
         
         /// <summary>
@@ -58,6 +60,27 @@ namespace NuGet.Services.EndToEnd
             await _clients.FlatContainer.WaitForPackageAsync(package.Id, package.NormalizedVersion, _logger);
             var licenseFileList = await _clients.FlatContainer.TryAndGetFileStringContent(package.Id, package.NormalizedVersion, FlatContainerStringFileType.License, _logger);
             Assert.All(licenseFileList, x => Assert.Equal(package.Properties.LicenseFileContent, x));
+            VerifyLicenseUrlForV2V3Search(package, expectedPath);
+        }
+
+        private async void VerifyLicenseUrlForV2V3Search(Package package, string expectedPath)
+        {
+            await _clients.V2V3Search.WaitForPackageAsync(package.Id, package.FullVersion, _logger);
+            var searchServices = await _clients.V2V3Search.GetSearchServicesAsync(_logger);
+
+            foreach (var searchService in searchServices)
+            {
+                var results = await _clients.V2V3Search.QueryAsync(
+                    searchService,
+                    $"q=packageid:{package.Id}",
+                    _logger);
+
+                Assert.True(results.Data.Count > 0);
+                for (var i = 0; i < results.Data.Count; i++)
+                {
+                    Assert.Equal(expectedPath, new Uri(results.Data[i].LicenseUrl).AbsolutePath);
+                }
+            }
         }
     }
 }
